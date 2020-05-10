28ce87665d30877bf0e46edf9fa84f2eb3bf7ca9
sana <sgolrang@myseneca.ca>
2020-05-10 11:23:22 -0400

    First commit

--- src/Persons/Person/Person.js
+++ src/Persons/Person/Person.js
@@ -1,25 +1,27 @@
 import React, { Component } from 'react';
-// import Auxiliary from '../../hoc/Auxiliary';
+
+import Auxiliary from '../../hoc/Auxiliary';
 import withClass from '../../hoc/withClass';
 import classes from './Person.css';
 
 class Person extends Component {
-  render(){
-    console.log('[Person.js] rendering');
-    return(
-      <React.Fragment>
-         <p onClick ={ this.props.click}>
-      I'm {this.props.name} and I am {this.props.age} years old!
-      </p>
-      <p key="i2">{this.props.children}</p>
-      <input 
-      key= "i3"
-      type = "text"
-      onChange={this.props.changed}
-      value= {this.props.name}
-      />
-      </React.Fragment>
-    )
+  render() {
+    console.log('[Person.js] rendering...');
+    return (
+      <Auxiliary>
+        <p onClick={this.props.click}>
+          I'm {this.props.name} and I am {this.props.age} years old!
+        </p>
+        <p key="i2">{this.props.children}</p>
+        <input
+          key="i3"
+          type="text"
+          onChange={this.props.changed}
+          value={this.props.name}
+        />
+      </Auxiliary>
+    );
   }
 }
+
 export default withClass(Person, classes.Person);
